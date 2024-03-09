a9f7cb3ed19c0a28f2f727b48817ae8ffe2ac352
ymougenel <ymougenel@takima.fr>
2024-03-09 21:26:03 +0100

    feat: init project

---
+++ main.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([48.866667, 2.333], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
