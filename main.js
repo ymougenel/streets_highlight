6de908042714557da5c23229b1401032fc47db76
ymougenel <ymougenel@takima.fr>
2024-04-23 22:32:09 +0200

    fix: map in FR, add github logo & switch res color

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const baselayers = {};
 // Creating one tile layers for each style
 styles.forEach((style) =>
   baselayers[style] = L.tileLayer(
-    `https://tile.jawg.io/${style}/{z}/{x}/{y}.png?access-token=${accessToken}`, {
+    `https://tile.jawg.io/${style}/{z}/{x}/{y}.png?lang=fr&access-token=${accessToken}`, {
       attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank" class="jawg-attrib">&copy; <b>Jawg</b>Maps</a> | <a href="https://www.openstreetmap.org/copyright" title="OpenStreetMap is open data licensed under ODbL" target="_blank" class="osm-attrib">&copy; OSM contributors</a>',
     }
   )
@@ -108,7 +108,7 @@ function choose_color(category) {
     if (! category) {
         return "#002aff";
     }
-    colors = [ "#56CBF9", "#1c2833", "#e6b800","#5b2c6f","#b34700","#b30059","#264d00","#86592d","#5D737E", "#87BBA2", "#435058", "#EAC5D8","#3C1518","#B370B0", "#002aff", "#031D44", "#87255B","#5C415D", "#E94F37", "#FC9E4F", "#344966"]
+    colors = [ "#56CBF9", "#1c2833", "#e6b800","#5b2c6f","#b34700","#b30059","#264d00","#86592d","#ff0000", "#87BBA2", "#435058", "#EAC5D8","#3C1518","#B370B0", "#002aff", "#031D44", "#87255B","#5C415D", "#E94F37", "#FC9E4F", "#344966"]
     content = mapped_colors.get(category);
     if (! content) { // Unknown category
         color = colors[mapped_colors.size % colors.length]
