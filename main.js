b75f8e736950d6922c75f3c2c8f1fe6b86bd1e4d
ymougenel <ymougenel@takima.fr>
2024-04-28 22:17:34 +0200

    style: last adjustements

--- main.js
+++ main.js
@@ -122,7 +122,7 @@ function choose_color(category) {
     if (! category) {
         return "#002aff";
     }
-    colors = [ "#56CBF9", "#1c2833", "#e6b800","#5b2c6f","#b34700","#b30059","#264d00","#86592d","#ff0000", "#87BBA2", "#435058", "#EAC5D8","#3C1518","#B370B0", "#002aff", "#031D44", "#87255B","#5C415D", "#E94F37", "#FC9E4F", "#344966"]
+    colors = [ "#078fc5", "#1c2833", "#e6b800","#5b2c6f","#ff6600","#008000","#264d00","#ff0000","#86592d", "#002aff", "#435058", "#EAC5D8","#3C1518","#B370B0", "#031D44", "#87255B","#5C415D", "#E94F37", "#FC9E4F", "#344966"]
     content = mapped_colors.get(category);
     if (! content) { // Unknown category
         color = colors[mapped_colors.size % colors.length]
